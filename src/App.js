39a4130a6dd2baeebea77cfe76340667a853f377
wojtekpac <w.pac@meatnet.pl>
2017-12-12 17:12:24 +0100

    fetched data not in array :/

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 // import ToolTip from './ToolTip';
 // import InfoBox from './InfoBox';
 import Header from './Header';
+import ListItem from './ListItem';
 
 
 class App extends Component {
@@ -26,29 +27,36 @@ class App extends Component {
         this.getData()
             .then((Response) => Response.json())
             .then((data) => {
-                const sortedData = [];
+                const dupa = data.bpi;
 
-                console.log(Response.json);
-                console.log(data.bpi);
-
+                let sortedData = dupa.map(a => a.foo);
 
                 this.setState({
                     data: sortedData,
                     fetchingData: false
                 })
-
+                // console.log(data.bpi) //ok
+                // console.log(sortedData)
+                // console.log(this.state.data)  // empty array
             })
             .catch(function (err) {
                 console.log(err);
             })
     }
 
+    renderBitcoinHistory() {
+        return  <div>tiruriru</div>
+
+    }
+
     render() {
+        const {data} = this.state;
         return (
             <div className="App">
                 <Header/>
-
-
+                {this.renderBitcoinHistory()}
+                <ListItem/>
+                <div>{data}</div>
             </div>
         );
     }
---
+++ src/ListItem.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wojciechpac on 12.12.2017.
+ */
+import React from 'react';
+
+
+const ListItem = (props) => {
+    return <li>sdfsf</li>;
+}
+
+export default ListItem;
